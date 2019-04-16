acc8fc3ac651db224cd435a280b5e7a1a7666d9c
leonidbondarenko69 <leonidbondarenko69@gmail.com>
2019-04-17 00:32:42 +0300

    Add files via upload

---
+++ int(input).py
@@ -0,0 +1,7 @@
+lenght = int(input('Введите длину участка: '))
+breadth = int(input('Введите ширину участа: '))
+
+area = lenght * breadth
+print('Площадь равна', area)
+print('Периметр равен', 2*(lenght + breadth))
+print('А самому слабо посчитать?')
